5789ba30eab6352ad2c8e15d0d4ddd6b796ce83f
alexandrelaranja <alexandrelaranja.souza@gmail.com>
2013-03-31 16:06:55 -0300

    Adicionando Classe com Egit.

---
+++ TesteGit/src/Projeto.java
@@ -0,0 +1,12 @@
+
+public class Projeto {
+	private String descricao;
+	
+	public String getDescricao(){
+		return descricao;
+	}
+	
+	public void setDescricao(String descricao){
+		this.descricao = descricao;
+	}
+}
